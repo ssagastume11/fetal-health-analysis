f7fdab0400bc559eb9d177399fb47b803d1ecf25
ssagastume11 <sagassergio123@gmail.com>
2026-02-14 14:54:25 -0500

    BigQuery SQL queries

--- SQL/Step 3_ Process.sql
+++ SQL/Step 3_ Process.sql
@@ -1,1 +1,19 @@
+-- Objective:
 
+-- The goal of this step is to transform the cleaned fetal hetal dataset into
+-- meaningful analytical summaries that allow for comparison across fetal hetal
+-- classifications.
+--This includes aggregating CTG features, calculating descriptive statistics,
+-- and structuring the data to uncover patterns that distinguish Normal, Suspect, 
+-- and Pathological fetal health outcomes
+
+-- Key Actions:
+
+-- * Aggregate CTG indicators by fetal health classication.
+-- * Compute summary statistics (e.g., averages, ranges, counts) for key features.
+-- * Compare fetal heart rate, variability, accelerations, and decelerations
+-- across health categories.
+-- * Prepare grouped and summarized tables to support trend analysis,
+-- visualization, and interpretation.
+-- * Create analysis-level views or tables that highlight differences
+-- between low-risk and high-risk fetal health cases.
